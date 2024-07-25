8cca00d3469935f88926ab2b84fc49185230738c
GRM-R-D <simran.aujla@grm-uk.com>
2024-07-25 14:14:52 +0100

    Update 1_📈Postcode Data.py

--- pages/1_📈Postcode Data.py
+++ pages/1_📈Postcode Data.py
@@ -154,7 +154,7 @@ if st.session_state.selected_geology_code:
 
 # Row 2: Legend and Checkboxes
 with row2[0]:
-    legend_html = """<div style="position: fixed; bottom: 10px; left: 10px; width: auto; height: 80px; 
+    legend_html = """<div style="position: fixed; bottom: 10px; left: 10px; width: auto; height: 50px; 
     background-color: black; border:2px solid grey; border-radius:8px; z-index:9999; font-size:14px; padding: 10px; 
     white-space: nowrap; font-family: 'Arial', sans-serif; color: #FAFAFA;"> <b style="font-family: 'Arial', 
     sans-serif; display: block; margin-bottom: 10px;">Plasticity Index</b> <i style="background:green; width: 20px; 
