43e434ee1f2ad9ea6f7749eb9fff6d8f8cee073c
GRM-R-D <simran.aujla@grm-uk.com>
2024-07-25 14:15:15 +0100

    Update 1_📈Postcode Data.py

--- pages/1_📈Postcode Data.py
+++ pages/1_📈Postcode Data.py
@@ -156,10 +156,10 @@ if st.session_state.selected_geology_code:
 with row2[0]:
     legend_html = """<div style="position: fixed; bottom: 10px; left: 10px; width: auto; height: 50px; 
     background-color: black; border:2px solid grey; border-radius:8px; z-index:9999; font-size:14px; padding: 10px; 
-    white-space: nowrap; font-family: 'Arial', sans-serif; color: #FAFAFA;"> <b style="font-family: 'Arial', 
-    sans-serif; display: block; margin-bottom: 10px;">Plasticity Index</b> <i style="background:green; width: 20px; 
-    height: 20px; display: inline-block; margin-right: 5px;"></i> < 10 <i style="background:yellow; width: 20px; 
-    height: 20px; display: inline-block; margin-right: 5px; margin-left: 10px;"></i> 10 - 20 <i 
+    margin-top: 10px; white-space: nowrap; font-family: 'Arial', sans-serif; color: #FAFAFA;"> <b style="font-family: 
+    'Arial', sans-serif; display: block; margin-bottom: 10px;">Plasticity Index</b> <i style="background:green; 
+    width: 20px; height: 20px; display: inline-block; margin-right: 5px;"></i> < 10 <i style="background:yellow; 
+    width: 20px; height: 20px; display: inline-block; margin-right: 5px; margin-left: 10px;"></i> 10 - 20 <i 
     style="background:orange; width: 20px; height: 20px; display: inline-block; margin-right: 5px; margin-left: 
     10px;"></i> 20 - 40 <i style="background:red; width: 20px; height: 20px; display: inline-block; margin-right: 
     5px; margin-left: 10px;"></i> ≥ 40 </div>"""
