c845070a3135809ef0986cdc9e518d0a35ab14ae
GRM-R-D <simran.aujla@grm-uk.com>
2024-07-30 11:12:12 +0100

    Update 3_Graphs.py

--- pages/3_Graphs.py
+++ pages/3_Graphs.py
@@ -1,16 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-# Load the CSV data
-data = pd.read_csv('Pointdate.csv')
+DataFrame = 'Pointdate.csv'
 
-# Display the data (optional)
-st.write("Data from CSV:")
-st.write(data)
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Date", "PlasticityIndex"])
 
-# Create the line chart
-st.line_chart(data)
-
-# If you want to customize the x and y axes, you can specify the columns
-# For example, if your CSV has columns 'Date' and 'Value'
-# st.line_chart(data.set_index('Date')['Value'])
+st.line_chart(chart_data)
