99476e57c94b8946b35d899f948790b6334af684
GRM-R-D <simran.aujla@grm-uk.com>
2024-07-30 11:10:29 +0100

    Update 3_Graphs.py

--- pages/3_Graphs.py
+++ pages/3_Graphs.py
@@ -1,5 +1,16 @@
-from st_aggrid import AgGrid
+import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('Pointdate.csv')
-AgGrid(df)
+# Load the CSV data
+data = pd.read_csv('Pointdate.csv')
+
+# Display the data (optional)
+st.write("Data from CSV:")
+st.write(data)
+
+# Create the line chart
+st.line_chart(data)
+
+# If you want to customize the x and y axes, you can specify the columns
+# For example, if your CSV has columns 'Date' and 'Value'
+# st.line_chart(data.set_index('Date')['Value'])
