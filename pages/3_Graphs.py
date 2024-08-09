0f0a6a8bcff5f0a1dfc45eb7198357aa72051a82
GRM-R-D <simran.aujla@grm-uk.com>
2024-08-09 16:31:59 +0100

    Update 3_Graphs.py

--- pages/3_Graphs.py
+++ pages/3_Graphs.py
@@ -1,9 +1,10 @@
 import streamlit as st
-import plotly.express as px
+from bokeh.plotting import figure
 
-df = px.data.iris()  # iris is a pandas DataFrame
-fig = px.scatter(df, x="sepal_width", y="sepal_length")
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
-event = st.plotly_chart(fig, key="iris", on_select="rerun")
+p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
+p.line(x, y, legend_label="Trend", line_width=2)
 
-event
+st.bokeh_chart(p, use_container_width=True)
