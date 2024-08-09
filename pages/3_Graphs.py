59a55c3d35d33ba733379d0098d09f8a2a682787
GRM-R-D <simran.aujla@grm-uk.com>
2024-08-09 16:41:26 +0100

    Update 3_Graphs.py

--- pages/3_Graphs.py
+++ pages/3_Graphs.py
@@ -15,9 +15,6 @@ filtered_data = data[data['GeologyCode'] == 'OADBY TILL MEMBER']
 count_data = filtered_data['PlasticityIndex'].value_counts().reset_index()
 count_data.columns = ['PlasticityIndex', 'Count']
 
-# Exclude Plasticity Index values with a count of 0 (though `value_counts` should not include zero counts)
-count_data = count_data[count_data['Count'] > 0]
-
 # Exclude Plasticity Index values that are 0
 count_data = count_data[count_data['PlasticityIndex'] != 0]
 
@@ -38,7 +35,10 @@ fig.update_layout(
     xaxis_title='Plasticity Index',
     yaxis_title='Count of Samples',
     title=dict(text='Plasticity Index vs. Count of Samples', x=0.5),
-    xaxis=dict(tickmode='linear')
+    xaxis=dict(
+        tickmode='linear',
+        dtick=1  # Set the increment of x-axis ticks. Change this value to adjust increments.
+    )
 )
 
 # Render the chart with Streamlit
