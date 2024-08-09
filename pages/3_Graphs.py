4d3f01d38046ef40e3bd84a0646a582c4cc772d9
GRM-R-D <simran.aujla@grm-uk.com>
2024-08-09 14:18:10 +0100

    Update 3_Graphs.py

--- pages/3_Graphs.py
+++ pages/3_Graphs.py
@@ -20,7 +20,7 @@ st.write(mean_data)
 
 # Prepare data for streamlit-lightweight-charts
 chart_data = mean_data[['Date', 'PlasticityIndex']].rename(columns={'Date': 'time', 'PlasticityIndex': 'value'})
-chart_data['time'] = chart_data['time'].astype(str)  # Convert datetime to string
+chart_data = chart_data.to_dict(orient='records')
 
 # Chart options
 chartOptions = {
